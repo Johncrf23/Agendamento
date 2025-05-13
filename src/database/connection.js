dfc92b6b209ba862b1c07834b86ae887fd68cd20
john da cp9 <pjonathandm@gmail.com>
2025-05-13 10:52:47 -0300

    V2

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,15 +2,10 @@ import mysql from "mysql2/promise"
 import dotenv from "dotenv";
 dotenv.config();
 
-async function connect() {
-const connection = await mysql.createConnection({
+const connection = await mysql.createPool({
 host: process.env.MYSQL_HOST,
 user: process.env.MYSQL_USER,
 password: process.env.MYSQL_PASSWORD,
 database: process.env.MYSQL_DATABASE
-});
-
-return connection;
-}
-
-export default { connect };
+})
+export default {connection};
