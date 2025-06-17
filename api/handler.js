1815964e6e530df25030b8b6d10dcebfeaf5e7ee
john da cp9 <pjonathandm@gmail.com>
2025-06-17 10:45:45 -0300

    V5.2

--- api/handler.js
+++ api/handler.js
@@ -1,3 +1,4 @@
-import serverless from 'serverless-http';
-import app from '../src/index.js';  
+import serverless from "serverless-http";
+import app from "./index.js";
+
 export const handler = serverless(app);
--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   const PORT = 3000;
   app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
