e05f62208942ce8d863e16418ceb2ec997af3e24
john da cp9 <pjonathandm@gmail.com>
2025-06-17 11:47:59 -0300

    V6.6

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,8 @@
+import serverless from "serverless-http";
+import app from "./index.js";
 
-import serverless from 'serverless-http';
-import app from './index.js';
+export const config = {
+  runtime: "nodejs18.x",
+};
 
 export const handler = serverless(app);
